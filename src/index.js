27c0866c6e1dba85479e8d20411e676f33708cbb
marmooo <marmooo@users.noreply.github.com>
2026-03-11 00:00:52 +0900

    improve dark-mode

--- src/index.js
+++ src/index.js
@@ -3,23 +3,15 @@ let problem = ["ASMR", "ASMR"];
 let paused = false;
 let englishVoices = [];
 let japaneseVoices = [];
-loadConfig();
 loadVoices();
 
-function loadConfig() {
-  if (localStorage.getItem("darkMode") == 1) {
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
-}
-
 function toggleDarkMode() {
-  if (localStorage.getItem("darkMode") == 1) {
-    localStorage.setItem("darkMode", 0);
-    document.documentElement.setAttribute("data-bs-theme", "light");
-  } else {
-    localStorage.setItem("darkMode", 1);
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
+  const html = document.documentElement;
+  const newTheme = html.getAttribute("data-bs-theme") === "dark"
+    ? "light"
+    : "dark";
+  html.setAttribute("data-bs-theme", newTheme);
+  localStorage.setItem("darkMode", newTheme);
 }
 
 function loadVoices() {
